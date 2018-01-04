5855200be96690143c173960afab77de8a05e640
olpig <pigeonolivier@hotmail.com>
2018-01-04 19:21:53 +0100

    Modifications by Mounah Bizri

--- classes.cpp
+++ classes.cpp
@@ -3,18 +3,65 @@
 
 namespace project
 {
+	underlying::underlying(std::vector<double> UndlData)
+		: m_UndlData(UndlData)
+	{
+	}
+	underlying::get_underlying(std::size_t UndlPos)
+	{
+		return m_UndlData[UndlPos];
+	}
+	underlying::~underlying()
+	{
+		delete[] m_UndlData;
+		m_UndlData= nullptr;
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
+	bevolatilities::bevolatilities(const std::size_t size)
+	{
+		m_bevols.resize(size); //Pas sur pour celle_ci
+	}
+	
+	bevolatilities::get_bevolatilities(std::size_t index) const
+	{
+		return m_bevols[index];
+	}
+	bevolatilities::set_bevolatilities(std::size_t index, double bevol) const
+	{
+		m_bevols[index]=bevol;
+	}
+	bevolatilities::~bevolatilities()
+	{
+		delete[] m_bevols;
+		m_bevols= nullptr;
+	}
+	rates::rates(std::vector<double> rates)
+		: m_rates(rates)
+	{
+	}
+	rates::get_rate(double maturity)
+	{
+		return m_rates[maturity]; //Je considère que la maturité est l'indexe, Il suffit juste de dire que 1st date=1 et voila
+	}
+	rates::~rates()
+	{
+		delete[] m_rates;
+		m_rates= nullptr;
+	}
+	double BSPricer(double undl, int time_to_mat, double strike,double rt ,double vol,normal_distribution norm_dist);
+	{
+		double d1;
+		double d2;
+		d1=(std::log(undl/strike)+(rt-vol*vol/2)*std::sqrt(time_to_mat))/(vol*std::sqrt(time_to_mat));
+		d2=d1-vol*std::sqrt(time_to_mat);
+		return undl*norm_dist(d1)-strike*std::exp(-rt*time_to_maturity)*norm_dist(d2);
+	}
+	double Delta(underlying undl, int time_to_mat, double strike,rate rt ,double vol,std::normal_distribution ndist);
+	{
+		double pi; //pi n'est pas explicitement défini sur C++
+		pi=3.14159265358979323846264338328;
+		double d1;
+		d1=(std::log(undl/strike)+(rt-vol*vol/2)*std::sqrt(time_to_mat))/(vol*std::sqrt(time_to_mat));
+		return 1/(std::sqrt(2*time_to_mat))*std::exp(-0.5*d1*d1);
+	}
 }
-
--- classes.hpp
+++ classes.hpp
@@ -3,47 +3,77 @@
 
 namespace project
 {
-	class volatility()
+	class bevolatilities()
 	{
+	//Pourquoi tu utilises du virtual? La classe n'est pas une classe mère, personne n'en hérite
 	public:
-		volatility(const std::vector<double>& vol = //insert code for vector of 0// );
-		virtual double get_volatility(std::size_t index) const; //read method
-		virtual double set_volatility(std::size_t index) const; //write method
-		virtual ~volatility();
-	protected:
+		bevolatilities(std::size_t size);//& vol = //insert code for vector of 0// ); Si on lui impose une valeur arbitraire on ne pourra plus la modifier après
+		//est-ce que le const va marcher si on veut le modifier après
+		//Le constructeur ne doit prendre que la taille du vecteur en param je crois
+		double get_bevolatility(std::size_t index) const; //read method
+		void set_bevolatility(std::size_t index, double bevol) const; //Question pour const encore car on veut modifier un vector qui est constant
+		//type void car on ne renvoie rien 
+		~bevolatilities();
+		//virtual double get_bevolatility(std::size_t index) const; //read method
+		//virtual double set_bevolatility(std::size_t index, double bevol) const; //write method
+		//virtual ~volatility();
+	//protected: //a-t-on besoin de protected si personne n'hérite de cette classe
 
 	private:
-		std::vector<double> m_volatility;
+		std::vector<double> m_bevols;
 	};
 
 
 	class rate()
 	{
 	public:
-
-		virtual ~rate();
+		virtual rates(std::vector<double> rates);
+		virtual double get_rate(double maturity) const;
+		virtual ~rates();
 	protected:
 
 	private:
-
+		std::vector<double> m_rates;
 	};
-
+	//Je me pose la question de la redondance de la classe flat_rate car 
+	//en soi flatrate= rate avec un vecteur de taille 1
 	class flat_rate() : public rate
 	{
 	public:
 		explicit flat_rate(double rate);
-		virtual double get_rate(double maturity) const;
+		virtual double get_rate() const;
+		//virtual double get_rate(double maturity) const;//SI c'est un taux fixe, pas besoin de la maturité
 		virtual ~flat_rate();
 	protected:
 
 	private:
 		double m_rate;
 	};
-
+	
+	class underlying()
+	{
+		public:
+			underlying(std::vector<double> UndlData);
+			double get_underlying(std::size_t UndlPos);
+			
+			~underlying();
+		
+		private:
+			std::vector<double> m_UndlData;
+	}
+	
+	
+	//Une classe pricer fait-elle vraiment sens en terme de mémoire
+	// Sachant que le time to mat évolue et qu'en soit cette classe
+	// ne contiendra que des fonctions
+	// Du coup peut-être qu'en terme de mémoire et de perf on sera gagnant en n'implémant que des fonctions
+	double BSPricer(double undl, int time_to_mat, double strike,rate rt ,double vol,std::normal_distribution norm_dist);
+	//double undl parce que je n'ai besoin que d'un undl précis
+	double Delta(underlying undl, int time_to_mat, double strike,rate rt ,double vol);	
 	class pricer()
 	{
 	public:
-		pricer();
+		pricer(underlying undl, int time_to_mat, double strike,double rt ,double vol);
 		double compute_delta();
 		double compute_price(); //is this really needed?
 
@@ -65,4 +95,3 @@ namespace project
 
 
 }
-
