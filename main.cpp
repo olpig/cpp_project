9750b489c135ea61b16c0bf62ea7e0f924053a71
olpig <pigeonolivier@hotmail.com>
2018-01-07 22:57:43 +0100

    cleaned repository, removed gnuplot code

--- main.cpp
+++ main.cpp
@@ -4,21 +4,6 @@
 #include "classes.hpp"
 #include "underlying.hpp"
 #include "ComputeBeVol.hpp"
-#include "gnuplot_i.hpp" //Gnuplot class handles POSIX-Pipe-communikation with Gnuplot
-
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__)
- #include <conio.h>   //for getch(), needed in wait_for_key()
- #include <windows.h> //for Sleep()
- void sleep(int i) { Sleep(i*1000); }
-#endif
-
-#define SLEEP_LGTH 2  // sleep time in seconds
-#define NPOINTS    50 // length of array
-
-void wait_for_key(); // Programm halts until keypress
-
-using std::cout;
-using std::endl;
 
 // To compile
 //
@@ -136,48 +121,6 @@ int main(int argc, char* argv[])
 			k += increment_k;
 		}
 	}
-
-	//
-    // Plotting Using the GnuplotException class
-    //
-	// FOR MICROSOFT if path-variable for gnuplot is not set, do it with:
-    //Gnuplot::set_GNUPlotPath("C:/Program Files/gnuplot/bin/");
-    //temporarily disable this part for testing
-	/*
-	try
-    {
-        Gnuplot g1("lines");
-		cout << endl << endl << "*** user-defined lists of points (x,y)" << endl;
-        g1.set_grid();
-        g1.set_style("points").plot_xy(v_AllStrikes,v_beVol,"user-defined points 2d");
-		g1.showonscreen();
-		wait_for_key();
-
-    }
-    catch (GnuplotException ge)
-    {
-        cout << ge.what() << endl;
-    }
-	*/
 		
 	return 0;
 }
-void wait_for_key ()
-{
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__)  // every keypress registered, also arrow keys
-    cout << endl << "Press any key to continue..." << endl;
-
-    FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
-    _getch();
-#elif defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
-    cout << endl << "Press ENTER to continue..." << endl;
-
-    std::cin.clear();
-    std::cin.ignore(std::cin.rdbuf()->in_avail());
-    std::cin.get();
-#endif
-    return;
-}
-
-
-
